034a1459f7d5ce86882046edfadc9a21c59e0e59
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2023-08-03 12:08:58 +0530

    Create Scanner.js

---
+++ src/Scanner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Qr from './Qr';
+function Scanner() {
+  return (
+    <div>
+    <h1 style={{textAlign:'center'}}>QR Code Generator App</h1>
+    <Qr />
+  </div>
+  )
+}
+
+export default Scanner
